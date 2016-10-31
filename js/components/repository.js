f2907a514ee13c8b7d6bab64939a4fa51095e992
Liz <elizabeth.m.carranza@gmail.com>
2016-10-31 03:49:26 +0000

    adding redux

---
+++ js/components/repository.js
@@ -0,0 +1,32 @@
+var React = require('react');
+
+var connect = require('react-redux').connect;
+
+var StarRater = require('./star-rater');
+
+var actions = require('../actions/index');
+
+var Repository = React.createClass({
+    changeRating: function(rating) {
+        // TODO: Change the rating
+        this.props.dispatch(
+            actions.rateRepository(this.props.repository.name, rating)
+        );
+    },
+    render: function() {
+        return (
+            <div className="repository">
+                {this.props.repository.name}
+                &nbsp;
+                <StarRater rating={this.props.repository.rating}
+                           onChange={this.changeRating} />
+            </div>
+        );
+    }
+});
+
+module.exports = Repository;
+
+var Container = connect()(Repository);
+
+module.exports = Container;
